ad25b8a5eb185124021e0418d6977206aa721ad8
aymeric.palaric@aaico.com <aymeric.palaric@tudent-cs.fr>
2023-10-13 09:00:42 +0200

    grid search + early stopping

--- src/Train/train_xgboost.py
+++ src/Train/train_xgboost.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
 
 from src.Preprocessing import *
 from config import *
@@ -10,12 +11,7 @@ from config import *
 
 def load_data(csv_name):
     csv_path = os.path.join(DATA_FOLDER, csv_name)
-    return pd.read_csv(csv_path, sep=';')
-
-
-def main():
-    # Chargement des données
-    df = load_data(DATA_FILENAME)
+    df = pd.read_csv(csv_path, sep=';')
     df = date_to_float_col(df, replace=False)
     df["depart_region"] = df["gare_depart"].apply(gare_region)
     df["arrivee_region"] = df["gare_arrivee"].apply(gare_region)
@@ -42,19 +38,28 @@ def main():
 
     X_train, y_train, X_test, y_test = train_test(df)
 
+    return X_train, y_train, X_test, y_test
+
+
+def train(X_train, y_train, X_test, y_test, **kwargs):
     xgb = XGBRegressor(
-        n_estimators=200,
-        verbosity=2
+        **kwargs
     )
 
-    xgb.fit(X_train, y_train)
-
+    xgb.fit(X_train, y_train, early_stopping_rounds=100,
+            eval_set=[(X_train, y_train), (X_test, y_test)
+                      ],
+            )
     score = xgb.score(X_train, y_train)
-    print("Trainging score:", score)
-    test_score = xgb.score(X_test, y_test)
-    print("Test score :", test_score)
+    print("Training score :", score)
+    return xgb
 
+
+def plot_test(xgb, X_test, y_test):
     y_pred = xgb.predict(X_test)
+    score = xgb.score(X_test, y_test)
+    print("Test score :", score)
+
     mse = mean_squared_error(y_test, y_pred)
     print("MSE :", mse)
 
@@ -64,6 +69,37 @@ def main():
     plt.title("Boston test and predicted data")
     plt.legend()
     plt.show()
+
+
+def grid_search(X_train, y_train, **grid):
+    gs_cv = GridSearchCV(
+        XGBRegressor(), grid, cv=5, refit=True, verbose=2
+    )
+    gs_cv.fit(X_train, y_train)
+    print(gs_cv.best_params_)
+    best_model = gs_cv.best_estimator_
+    return best_model
+
+
+def main():
+    # Chargement des données
+    X_train, y_train, X_test, y_test = load_data(DATA_FILENAME)
+
+    xgb = train(X_train, y_train, X_test, y_test,
+                n_estimators=10000,
+                max_depth=3,
+                learning_rate=0.01,
+                verbosity=2
+                )
+    # grid = {"n_estimators": [500, 1000, 1500],
+    #         "max_depth": [1, 3, 5, 10],
+    #         "learning_rate": [0.01],
+    #         "verbosity": [1]}
+    # xgb = grid_search(X_train, y_train,
+    #                   **grid
+    #                   )
+
+    plot_test(xgb, X_test, y_test)
 
 
 if __name__ == "__main__":

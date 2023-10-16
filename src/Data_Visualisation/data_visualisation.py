4ecc0934c1a20db3ac15e6179f233795cfcd7f8e
Revcolevschi Dan <dan.revcolevschi@student-cs.fr>
2023-10-16 23:28:23 +0200

    Top 5 des gares retardataires et en ratio

--- src/Data_Visualisation/data_visualisation.py
+++ src/Data_Visualisation/data_visualisation.py
@@ -47,7 +47,62 @@ def ratio_annulations_mensuel(fichier_csv):
     plt.legend()
     plt.grid(True)
     plt.show()
+def top_gares_retard(fichier_csv):
+    df = pd.read_csv(fichier_csv, sep=";")
+    df_date = date_to_float_col(df, replace=False)
+    annees = df_date['annee'].unique()
+    plt.figure(figsize=(12, 8))
+    for annee in annees:
+        subset = df_date[df_date['annee'] == annee]
+        total_retard_by_gare = subset.groupby('gare_depart')['retard_moyen_tous_trains_arrivee'].sum()
+        top_5_gares = total_retard_by_gare.sort_values(ascending=False).head(5)
+        
+        plt.bar(top_5_gares.index + f" ({annee})", top_5_gares.values, label=str(annee))
+        
+    # 4. Affichage des résultats
+    plt.title("Top 5 des gares avec le plus de retard par année")
+    plt.xlabel('Gares')
+    plt.ylabel('Total de retard (minutes, heures, etc. en fonction de l\'unité de mesure)')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.grid(axis='y')
+    plt.tight_layout()
+    plt.show()
+
+def ratio_retard_par_train(fichier_csv):
+    # 1. Lire le fichier CSV
+    df = pd.read_csv(fichier_csv, sep=";")
+    
+    # 2. Convertir les dates si nécessaire (en supposant que vous avez une fonction date_to_float_col)
+    df_date = date_to_float_col(df, replace=False)
+    annees = df_date['annee'].unique()
+    
+    plt.figure(figsize=(12, 8))
+    
+    # 3. Pour chaque année, trouvez le top 5 des gares basé sur le ratio retard par train prévu
+    for annee in annees:
+        subset = df_date[df_date['annee'] == annee]
+        total_retard_by_gare = subset.groupby('gare_depart')['retard_moyen_tous_trains_arrivee'].sum()
+        total_trains_by_gare = subset.groupby('gare_depart')['nb_train_prevu'].sum()
+        
+        ratio_retard = total_retard_by_gare / total_trains_by_gare
+        top_5_gares = ratio_retard.sort_values(ascending=False).head(5)
+        
+        plt.bar(top_5_gares.index + f" ({annee})", top_5_gares.values, label=str(annee))
+        
+    # 4. Affichage des résultats
+    plt.title("Top 5 des gares basé sur le ratio de retard par train prévu par année")
+    plt.xlabel('Gares')
+    plt.ylabel('Ratio de retard par train prévu')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.grid(axis='y')
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     nombre_annulations_mensuel("C:/Users/Dan/Downloads/aregularite-mensuelle-tgv-aqst.csv")
     ratio_annulations_mensuel("C:/Users/Dan/Downloads/aregularite-mensuelle-tgv-aqst.csv")
+    top_gares_retard("C:/Users/Dan/Downloads/aregularite-mensuelle-tgv-aqst.csv")
+    ratio_retard_par_train("C:/Users/Dan/Downloads/aregularite-mensuelle-tgv-aqst.csv")
